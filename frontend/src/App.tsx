29091f8d9caacb7fe525d1e9c9f007c1a024e82b
PyrozhenkoSerhii <serhii.pyrozhenko@nure.ua>
2022-03-26 19:31:29 +0200

    demo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useRef } from "react";
+import { io } from "socket.io-client";
+
+export const App = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if(!canvasRef.current) return;
+
+    const context = canvasRef.current?.getContext("2d");
+    const socket = io("localhost:8000");
+
+    socket.on("connect", () => {
+      console.log("connected", socket.id);
+    
+      socket.on("stream-data", (data) => {
+        console.log(data);
+
+        const array = new Uint8ClampedArray(data);
+        const image = new ImageData(array, 1280, 720);
+        context.putImageData(image, 0, 0);
+      })
+    });
+  }, [canvasRef.current]);
+
+  return (
+    <div>
+      <canvas
+        width={1280}
+        height={720}
+        ref={canvasRef}
+        style={{border: "1px solid gray", width: "480px"}}/>
+    </div>
+  )
+}
