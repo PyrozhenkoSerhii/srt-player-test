8df21331a45af8fd6f67c75300814a46d1366de7
PyrozhenkoSerhii <serhii.pyrozhenko@nure.ua>
2022-03-29 13:41:13 +0300

    audio handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
+import { AudioPlayer } from "./AudioPlayer";
+
+const audioPlayer = new AudioPlayer();
 
 export const App = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const toggleIsPlaying = () => {
+    setIsPlaying((prev) => !prev);
+    audioPlayer.togglePlayback();
+  }
 
   useEffect(() => {
     if(!canvasRef.current) return;
@@ -15,8 +25,7 @@ export const App = () => {
       console.log("connected", socket.id);
     
       socket.on("video-package", (data) => {
-        // console.log(data);
-        console.log(`Received a video-package of size: ${data.byteLength}`);
+        // console.log(`Received a video-package of size: ${data.byteLength}`);
 
         const array = new Uint8ClampedArray(data);
         const image = new ImageData(array, 1280, 720);
@@ -24,19 +33,15 @@ export const App = () => {
       })
 
       socket.on("audio-package", (data) => {
-        // console.log(data);
-
-        console.log(`Received an audio-package of size: ${data.byteLength}`);
-
-        // const array = new Uint8ClampedArray(data);
-        // const image = new ImageData(array, 1280, 720);
-        // context.putImageData(image, 0, 0);
+        // console.log(`Received an audio-package of size: ${data.byteLength}`);
+        audioPlayer.onData(data);
       })
     });
   }, [canvasRef.current]);
 
   return (
     <div>
+      <button onClick={toggleIsPlaying}>{isPlaying ? "Pause" : "Play"}</button>
       <canvas
         width={1280}
         height={720}
---
+++ frontend/src/AudioPlayer.ts
@@ -0,0 +1,102 @@
+
+import { float32Concat, getChannelsFromInterleave, int16ToFloat32BitPCM} from "./utils"
+
+export const BUFFER_SIZE = 2048;
+export const CHANNELS = 2;
+export const JITTER_BUFFER_SIZE = 10000;
+export const SAMPLE_RATE = 48000;
+
+export class AudioPlayer {
+  private audioQueueLeft: Float32Array = null;
+  private audioQueueRight: Float32Array = null;
+
+  private isPlaying = false;
+
+  private context: AudioContext = null;
+
+  constructor() {
+    this.initializeContext();
+  }
+
+  public togglePlayback = () => {
+    this.isPlaying = !this.isPlaying;
+    if(this.isPlaying && this.context.state === "suspended") {
+      this.context.resume(); 
+      console.log("[togglePlayback] context resumed");
+    } else if (!this.isPlaying && this.context.state === "running") {
+      this.context.suspend();
+      console.log("[togglePlayback] context suspended");
+    }
+    console.log("[togglePlayback] Status isPlaying: ", this.isPlaying);
+  }
+
+  private initializeContext = () => {
+    this.context = new AudioContext({ sampleRate: SAMPLE_RATE });
+    this.context.suspend();
+
+    const output = this.context.createScriptProcessor(BUFFER_SIZE, CHANNELS, CHANNELS);
+
+    output.onaudioprocess = (e) => {
+      if (this.audioQueueLeft && this.audioQueueLeft.length) {
+        const samplesToPlayLeft = this.audioQueueLeft.subarray(0, BUFFER_SIZE);
+        const samplesToPlayRight = this.audioQueueRight.subarray(0, BUFFER_SIZE);
+
+        this.audioQueueLeft = this.audioQueueLeft.subarray(BUFFER_SIZE, this.audioQueueLeft.length);
+        this.audioQueueRight = this.audioQueueRight.subarray(BUFFER_SIZE, this.audioQueueRight.length);
+
+        e.outputBuffer.getChannelData(0).set(samplesToPlayLeft);
+        e.outputBuffer.getChannelData(1).set(samplesToPlayRight);
+
+        console.log(`[onaudioprocess] Length of left queue: ${this.audioQueueLeft.length} right: ${this.audioQueueRight.length}`);
+      } else {
+        e.outputBuffer.getChannelData(0).set(new Float32Array(BUFFER_SIZE));
+        e.outputBuffer.getChannelData(1).set(new Float32Array(BUFFER_SIZE));
+
+        if (this.isPlaying) {
+          console.log("[onaudioprocess] STUTTERED");
+        }
+      }
+    };
+    output.connect(this.context.destination);
+  }
+
+  public onData = (data: ArrayBuffer) => {
+    const interleavedInt16Buffer = new Int16Array(data);
+
+    console.log(interleavedInt16Buffer);
+    const interleavedFloat32Buffer = int16ToFloat32BitPCM(interleavedInt16Buffer);
+
+    const {
+      leftChannel,
+      rightChannel,
+    } = getChannelsFromInterleave(interleavedFloat32Buffer);
+
+    if (this.isPlaying) {
+      if (this.audioQueueLeft && this.audioQueueRight) {
+        this.audioQueueLeft = float32Concat(this.audioQueueLeft, leftChannel);
+        this.audioQueueRight = float32Concat(this.audioQueueRight, rightChannel);
+      } else {
+        if (leftChannel.length < JITTER_BUFFER_SIZE) {
+          const leftInitialQueue = new Float32Array(JITTER_BUFFER_SIZE);
+          const rightInitialQueue = new Float32Array(JITTER_BUFFER_SIZE);
+
+          const zerosLength = JITTER_BUFFER_SIZE - leftChannel.length;
+
+          const zeroArray = new Float32Array(zerosLength);
+
+          leftInitialQueue.set(zeroArray);
+          rightInitialQueue.set(zeroArray);
+
+          leftInitialQueue.set(leftChannel, zerosLength);
+          rightInitialQueue.set(rightChannel, zerosLength);
+
+          this.audioQueueLeft = leftInitialQueue;
+          this.audioQueueRight = rightInitialQueue;
+        } else {
+          this.audioQueueLeft = leftChannel;
+          this.audioQueueRight = rightChannel;
+        }
+      }
+    }
+  }
+}
